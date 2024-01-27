e8754d50685456aac8fb7f36e481a19c32e00924
Abhijeet Sharma <abhijeet.vishu@gmail.com>
2024-01-27 17:31:30 +0530

    responsive fixes

--- components/Project.tsx
+++ components/Project.tsx
@@ -30,12 +30,12 @@ const Project: React.FC<IProjectProps> = props => {
     >
       <section
         className="bg-gray-100 mx-w-[42rem] border border-black/5 overflow-hidden sm:pr-8
-      relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 group-even:pl-8 hover:bg-gray-200 transition
+      relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 sm:group-even:pl-8 hover:bg-gray-200 transition
       rounded-lg dark:bg-white/10 dark:hover:bg-white/20 dark:text-white"
       >
         <div
           className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%]
-        flex flex-col h-full group-even:ml-[18rem]"
+        flex flex-col h-full sm:group-even:ml-[18rem]"
         >
           <h3 className="text-2xl font-semibold">{title}</h3>
 
